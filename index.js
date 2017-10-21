d7f558e89904ad5f374bab98329709b54299baa3
Max Zelinka <fourthwalljoke@gmail.com>
2017-10-21 02:52:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,10 @@ function sayHiToGrandma(string) {
   var uppercase = string.toUpperCase()
   var lowercase = string.toLowerCase()
   if(string === uppercase) {
-    return "I can\'t hear you!"
-  } else if (string === lowercase) {
     return "YES INDEED!"
+  } else if (string === lowercase) {
+    return "I can\'t hear you"
+  } else if (string === "I love you, Grandma.") {
+    return "I love you, too"
   }
 }
