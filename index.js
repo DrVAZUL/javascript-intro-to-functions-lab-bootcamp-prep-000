d14925d3efcef10deb501af3415b7b0d7e50405d
Max Zelinka <fourthwalljoke@gmail.com>
2017-10-21 02:49:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ function shout(string) {
 function whisper(string) {
   return string.toLowerCase()
 }
+
+function logShout(string) {
+  console.log(string.toUpperCase())
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  var uppercase = string.toUpperCase
+  var lowercase = string.toLowerCase
+  if(string === uppercase) {
+    return "I cant hear you!"
+  } else if (string === lowercase) {
+    return "YES INDEED!"
+  }
+}
